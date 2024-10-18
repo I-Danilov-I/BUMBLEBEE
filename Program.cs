ace761ba8b914df30b35efba08da520cb8ca0ba6
Anatolius <danilov@mail.de>
2024-10-18 10:27:59 +0200

    Update

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
             Browser_Control IBC = new();
+            
             AutoGiftCode.Start(IBC);
                
             //RuTube_Operations.Login(BC);            
